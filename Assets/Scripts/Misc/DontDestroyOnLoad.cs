1072bb6429b81ecba5e68f6a0fab8ae40133dc5f
Xtzzzz <76842783+qhdsfans@users.noreply.github.com>
2025-09-19 00:51:47 +0800

    Delete Assets directory

--- Assets/Scripts/Misc/DontDestroyOnLoad.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-#nullable enable
-public class DontDestroyOnLoad : MonoBehaviour
-{
-    private void Start()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-}
--- Assets/StreamingAssets/README.md
+++
@@ -1,4 +0,0 @@
-### ffarguments guide
-
-prepend `-report` flag to write the log out to `out.log`.
-further configuration of ffmpeg related arguments can refer to it's manual.
